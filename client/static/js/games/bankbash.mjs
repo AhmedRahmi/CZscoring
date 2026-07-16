70c862eba136a38eb24f465b761fac6f4efc4db2
AhmedRahmi <arthehaxxor@gmail.com>
2026-07-15 21:04:46 -0400

    I think scoring is done?

--- client/static/js/games/bankbash.mjs
+++ client/static/js/games/bankbash.mjs
@@ -7,7 +7,7 @@ export const bankbash = new ScoresheetTemplate()
 		.addObjective({
 			name: "Left Starting Area?",
 			type: "dropdown",
-			value: [0, 2],
+			value: [0, 5],
 			options: ["Didn't Leave", "Left"]
 		})
 		.addObjective({
@@ -19,26 +19,43 @@ export const bankbash = new ScoresheetTemplate()
 	.createSection("TeleOp") // teleop
 		.addObjective({
 			name: "Silver Coins bagged",
-			value: 4
+			value: 6
 		})
 		.addObjective({
 			name: "Gold Coins bagged",
-			value: 8
+			value: 10
 		})
 		.addObjective({
 			name: "Gold Bars bagged",
-			value: 12
+			value: 16
 		})
 	.createSection("End Game") // end game
 		.addObjective({
 			name: "Gold Blocks bagged",
-			value: 30
+			value: 25
 		})
 		.addObjective({
 			name: "Any Parking (Base Zone)?",
 			type: "dropdown",
-			value: [0, 2, 5],
+			value: [0, 3, 10],
 			options: ["Didn't Park", "Partially Parked", "Fully Parked"]
+		})
+		.createSection("Tagged Goods (You must talk with your fellow scorer for these)") // teleop
+		.addObjective({
+			name: "[OTHER TEAM] Tagged Coins Bagged",
+			value: 8
+		})
+		.addObjective({
+			name: "[OTHER TEAM] Tagged Gold Bars Bagged",
+			value: 12
+		})
+		.addObjective({
+			name: "Tagged Coins Left in Bagging",
+			value: 8
+		})
+		.addObjective({
+			name: "Tagged Bars Left in Bagging",
+			value: 12
 		})
 	.createSection("Penalties (You must talk with your fellow scorer for these)")
 		.addObjective({
